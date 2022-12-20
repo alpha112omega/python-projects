873c39c66a91e7ab19578c488a97fca56cb45574
Akshat Yadav <akshatyadav815@gmail.com>
2022-12-20 17:15:37 +0000

    Add files via upload
    program to get smallest amongst all the given number

---
+++ test.py
@@ -0,0 +1,16 @@
+num1=int(input("enter a value to start:"))
+num2=int(input("enter second value to start:"))
+num3=int(input("enter third valur to start:"))
+
+if(num1==num2==num3):
+  print("fuck your ass")
+elif(num1<num2 and num1<num3):
+  print("num1 is samllest")
+elif(num2<num1 and num3):
+  print("num2 is smallest")
+else:
+  print("num3 is the smallest")
+
+
+
+
