00481e2305eef1460211273107596fcb3167d423
Akshat Yadav <akshatyadav815@gmail.com>
2022-12-20 17:13:48 +0000

    Add files via upload
    program for calculating if three values are even or odd and giving output in accordance

---
+++ second.py
@@ -0,0 +1,8 @@
+num1=int(input("enter a value to start :"))
+num2=int(input("enter the second value :"))
+num3=int(input("enter the third value to start :"))
+if(num1 % 2==0 and num2 %2==0 and num3 %2==0):
+    print(" all num are even")
+    
+else:
+    print(" all numbers are not even")
